922bb7aef432d9bf2579f20cf4aab400415a7dac
Kailash Kumar <202051093@iiitvadodara.ac.in>
2025-06-22 21:39:10 +0530

    setup

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div className='text-2xl underline'>
+      <h1>Welcome to the Next.js App!</h1>
+      <Button>
+        Click Me
+      </Button>
+    </div>
+  )
+}
+
+export default Page
