1c4c2511caf344f9966a94523377c85ce8c21d10
Kailash Kumar <202051093@iiitvadodara.ac.in>
2025-07-05 20:34:36 +0530

    implement navbar

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,6 @@ import React from 'react'
 const Page = () => {
   return (
     <div className='text-2xl underline'>
-      <h1>Welcome to the Next.js App!</h1>
-      <Button>
-        Click Me
-      </Button>
     </div>
   )
 }
---
+++ components/Navbar.tsx
@@ -0,0 +1,25 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+import NavItems from './NavItems'
+
+const Navbar = () => {
+    return (
+        <nav className='navbar'>
+            <Link href={'/'}>
+                <Image
+                    src={'/images/logo.svg'}
+                    alt="logo"
+                    width={46}
+                    height={44}
+                />
+            </Link>
+            <div className='flex items-center gap-8'>
+                <NavItems />
+                <p>Sign In</p>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
