53180d27e8d5930abf8d7e5239ff148b06295262
Kailash Kumar <202051093@iiitvadodara.ac.in>
2025-07-09 11:45:25 +0530

    implemented auth, billing, database setup, create companion server action

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,48 @@
-import { Button } from '@/components/ui/button'
+import CompanionCard from '@/components/CompanionCard'
+import CompanionsList from '@/components/CompanionsList'
+import CTA from '@/components/CTA'
+import { recentSessions } from '@/constants'
 import React from 'react'
 
 const Page = () => {
   return (
-    <div className='text-2xl underline'>
-    </div>
+    <main>
+      <h1>Popular Companions</h1>
+      <section className='home-section'>
+        <CompanionCard
+          id="1"
+          name="Companion One"
+          topic="Topic One"
+          subject="Subject One"
+          duration={45}
+          color="#ffda6e"
+        />
+        <CompanionCard
+          id="2"
+          name="Companion two"
+          topic="Topic two"
+          subject="Subject two"
+          duration={30}
+          color="#e5d0ff"
+        />
+        <CompanionCard
+          id="3"
+          name="Companion three"
+          topic="Topic three"
+          subject="Subject three"
+          duration={30}
+          color="#bde7ff"
+        />
+      </section>
+      <section className='home-section'>
+        <CompanionsList
+          title="Recently Completed Sessions"
+          companions={recentSessions}
+          classNames="w-2/3 max-lg:w-full"
+        />
+        <CTA />
+      </section>
+    </main>
   )
 }
 
